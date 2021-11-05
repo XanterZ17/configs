c3d0ff2cef722652228372929dec17be5d4c9b5e
Alexnder <a.nekrasov7517@gmail.com>
2021-11-05 09:55:36 +0300

    add zsh and os-prober to base.sh

--- base.sh
+++ base.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-HOSTNAME = dumbass
-USERNAME = dumbass
-PASSWORD = dumbass
+HOSTNAME=dumbass
+USERNAME=dumbass
+PASSWORD=dumbass
 
 ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
 hwclock --systohc
 sed -i '177s/.//' /etc/locale.gen
 sed -i '403s/.//' /etc/locale.gen
+sed -i '160s/.//' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 echo $HOSTNAME >> /etc/hostname
@@ -15,9 +16,10 @@ echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1	localhost" >> /etc/hosts
 echo root:$PASSWORD | chpasswd
 
-pacman -S --noconfirm grub efibootmgr networkmanager base-devel openssh os-prober man
+pacman -S --noconfirm grub efibootmgr networkmanager base-devel openssh os-prober man zsh
 
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+echo 'GRUB_DISABLE_OS_PROBER=false' >> /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 
 systemctl enable NetworkManager
@@ -26,6 +28,10 @@ useradd -m $USERNAME
 echo $USERNAME:$PASSWORD | chpasswd
 chsh -s /usr/bin/zsh $USERNAME
 
+cp .zshrc /home/$USERNAME
+
 echo "$USERNAME ALL=(ALL) ALL" >> /etc/sudoers.d/$USERNAME
 
-printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
+localectl set-x11-keymap us,ru "" "" grp:alt_shift_toggle
+
+printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m\n"
